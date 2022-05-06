bf06f2a1276c1a4505934d57d96c05265dac8e86
Giltaehyeong <r1605866@naver.com>
2022-05-06 17:18:16 +0900

    MemberController.java 수정 기존 @RequestMapping 어노테이션 수정 @RequestMapping(value="경로", method=RequestMethod.GET) @RequestMapping(value="경로", method=RequestMethod.POST)
    이렇게 되어 있던거
    @GetMapping(value = "/idCheck")
    @PostMapping(value = "/login")
    이렇게 수정. 코드의 간결성을 위함!
    
    + 설명 주석문 제거
    + SQL문 } 빠져있는거 수정

--- src/main/java/com/odc/main/MainController.java
+++ src/main/java/com/odc/main/MainController.java
@@ -11,9 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.odc.utils.SessionMgr;
 
 @Controller
-public class MainController {		// 해당 package를 사용할려면 "\src\main\webapp\WEB-INF\spring\appServlet\servlet-context.xml" xml파일에
-											// 최하단부 context:component-scan의 base-package 부분을 추가하여야 한다.
-											// ex) base-package="com.odc.member" => base-package="com.odc.member, com.odc.main"
+public class MainController {		
 
 	// 메인 페이지 시작
 	@RequestMapping(value = "/")
@@ -29,11 +27,8 @@ public class MainController {		// 해당 package를 사용할려면 "\src\main\w
 	public ModelAndView header(HttpServletRequest request) {
 		
 		ModelAndView mav = new ModelAndView();
-		
-		// 세션안의 있는 모든 값을 Map자료형으로 return
 		Map<String, Object> sessMap = SessionMgr.outputData(request);
-		
-		// sessMap에 자료가 있을 경우 = 로그인이 되어있을 경우
+
 		if ( sessMap != null && !sessMap.isEmpty()) {
 			mav.addObject("sessData", sessMap);
 		}
@@ -57,10 +52,8 @@ public class MainController {		// 해당 package를 사용할려면 "\src\main\w
 	public ModelAndView myPage(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 		
-		// 세션안의 있는 모든 값을 Map자료형으로 return
 		Map<String, Object> sessMap = SessionMgr.outputData(request);
 		
-		// sessMap에 자료가 있을 경우 = 로그인이 되어있을 경우
 		if ( sessMap != null && !sessMap.isEmpty()) {
 			mav.addObject("sessData", sessMap);
 		}
@@ -68,5 +61,6 @@ public class MainController {		// 해당 package를 사용할려면 "\src\main\w
 		mav.setViewName("/sign/MyPage");
 		return mav;
 	}
-	// 마이페이지 끝
+
+	
 }
